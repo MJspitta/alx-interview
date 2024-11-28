898cc0f498bbe9ef3c78d46b5ac8613e070a1c43
Madu Jang <madu.jang93@gmail.com>
2024-11-28 20:03:08 +0100

    making changes

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" determine fewest number of coins needed to meet
+a given amount total
+"""
+
+
+def makeChange(coins, total):
+  """ calc how much change the total will require """
+  if total <= 0:
+    return 0
+  else:
+    coin = sorted(coins)
+    coin.reverse()
+    count = 0
+    for i in coin:
+      while(total >= i):
+        count += 1
+        total -= i
+    if total == 0:
+      return count
+    return -1
